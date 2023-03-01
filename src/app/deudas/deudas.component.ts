2c95af6a754d4e3878bf6e0e2cc4be2d08fa8b57
dmunoz <dmunoz@aligare.cl>
2023-03-01 11:42:44 -0300

    mas cambios

--- src/app/deudas/deudas.component.ts
+++ src/app/deudas/deudas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-deudas',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./deudas.component.css']
 })
 export class DeudasComponent {
+  constructor(private router: Router) { }
 
+  volver() {
+    this.router.navigate(['/mis-compras'])
+  }
 }
